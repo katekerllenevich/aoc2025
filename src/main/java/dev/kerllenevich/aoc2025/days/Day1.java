36d90dd90a753341bf01e495d8474b10e503d7f8
Kate Kerllenevich <kate@kerllenevich.dev>
2025-12-01 02:28:26 -0500

    day 1

--- src/main/java/dev/kerllenevich/aoc2025/days/Day1.java
+++ src/main/java/dev/kerllenevich/aoc2025/days/Day1.java
@@ -2,7 +2,9 @@ package dev.kerllenevich.aoc2025.days;
 
 import dev.kerllenevich.aoc2025.AbstractDay;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Day1 extends AbstractDay {
 
@@ -12,11 +14,49 @@ public class Day1 extends AbstractDay {
 
     @Override
     protected String part1() throws IOException {
-        throw new UnsupportedOperationException();
+        int pos = 50;
+        int zeroCount = 0;
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.charAt(0) == 'L') {
+                    pos -= Integer.valueOf(line.substring(1));
+                } else if (line.charAt(0) == 'R') {
+                    pos += Integer.valueOf(line.substring(1));
+                }
+
+                pos = ((pos % 100) + 100) % 100;
+                if (pos == 0) {
+                    zeroCount++;
+                }
+            }
+        }
+
+        return String.valueOf(zeroCount);
     }
 
     @Override
-    protected String part2() {
-        throw new UnsupportedOperationException();
+    protected String part2() throws IOException {
+        int pos = 50;
+        int zeroCount = 0;
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                boolean left = line.charAt(0) == 'L';
+
+                for (int times = Integer.valueOf(line.substring(1)); times > 0; times--) {
+                    pos += left ? -1 : 1;
+                    pos = ((pos % 100) + 100) % 100;
+
+                    if (pos == 0) {
+                        zeroCount++;
+                    }
+                }
+            }
+        }
+
+        return String.valueOf(zeroCount);
     }
 }
